a7672c0ccb30be93dce48c0ea4f816870e7b6f32
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-09-17 16:21:38 -0700

    Add docs

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,6 @@
 --!strict
+
+--- @class iter
 local iter = {}
 iter.__index = iter
 
@@ -46,51 +48,51 @@ function iter:__iter()
 	return iter._next, self
 end
 
---[[
-Tests if every element of the iterator matches a predicate.
+--[=[
+	Tests if every element of the iterator matches a predicate.
 
-`all()` takes a closure that returns true or false. It applies this closure
-to each element of the iterator, and if they all return true, then so does
-all(). If any of them return false, it returns false.
+	`all()` takes a closure that returns true or false. It applies this closure
+	to each element of the iterator, and if they all return true, then so does
+	all(). If any of them return false, it returns false.
 
-`all()` is short-circuiting; in other words, it will stop processing as soon
-as it finds a false, given that no matter what else happens, the result will
-also be false.
+	`all()` is short-circuiting; in other words, it will stop processing as soon
+	as it finds a false, given that no matter what else happens, the result will
+	also be false.
 
-An empty iterator returns true.
+	An empty iterator returns true.
 
-# Examples
+	# Examples
 
-Basic usage:
+	Basic usage:
 
-```lua
-local a = {1, 2, 3}
+	```lua
+	local a = {1, 2, 3}
 
-assert(iter(a):all(function(x)
-	return x > 0
-end))
+	assert(iter(a):all(function(x)
+		return x > 0
+	end))
 
-assert(not iter(a):all(function(x)
-	return x > 2
-end))
-```
+	assert(not iter(a):all(function(x)
+		return x > 2
+	end))
+	```
 
-Stopping at the first false:
+	Stopping at the first false:
 
-```lua
-local a = {1, 2, 3}
+	```lua
+	local a = {1, 2, 3}
 
-local iterator = iter(a)
+	local iterator = iter(a)
 
-assert(not iterator:all(function(x)
-	return x ~= 2
-end))
+	assert(not iterator:all(function(x)
+		return x ~= 2
+	end))
 
--- we can still use `iter`, as there are more elements.
-assert(next(iterator) == 3)
-```
-]]
-function iter:all(predicate: (any, any) -> boolean)
+	-- we can still use `iter`, as there are more elements.
+	assert(next(iterator) == 3)
+	```
+]=]
+function iter:all(predicate: (...any) -> boolean)
 	for key, value in self do
 		if predicate(self:_getInputTuple(key, value)) then
 			return false
@@ -100,51 +102,51 @@ function iter:all(predicate: (any, any) -> boolean)
 	return true
 end
 
---[[
-Tests if any element of the iterator matches a predicate.
+--[=[
+	Tests if any element of the iterator matches a predicate.
 
-`any()` takes a function that returns `true` or `false`. It applies this
-function to each element of the iterator, and if any of them return
-`true`, then so does `any()`. If they all return `false`, it returns `false`.
+	`any()` takes a function that returns `true` or `false`. It applies this
+	function to each element of the iterator, and if any of them return
+	`true`, then so does `any()`. If they all return `false`, it returns `false`.
 
-`any()` is short-circuiting; in other words, it will stop processing
-as soon as it finds a true, given that no matter what else happens,
-the result will also be true.
+	`any()` is short-circuiting; in other words, it will stop processing
+	as soon as it finds a true, given that no matter what else happens,
+	the result will also be true.
 
-An empty iterator returns `false`.
+	An empty iterator returns `false`.
 
-# Examples
+	# Examples
 
-Basic usage:
+	Basic usage:
 
-```lua
-local a = {1, 2, 3}
+	```lua
+	local a = {1, 2, 3}
 
-assert(iter(a):all(function(x)
-	return x > 0
-end))
+	assert(iter(a):all(function(x)
+		return x > 0
+	end))
 
-assert(not iter(a):all(function(x)
-	return x > 5
-end))
-```
+	assert(not iter(a):all(function(x)
+		return x > 5
+	end))
+	```
 
-Stopping at the first true:
+	Stopping at the first true:
 
-```lua
-local a = {1, 2, 3}
+	```lua
+	local a = {1, 2, 3}
 
-local iterator = iter(a)
+	local iterator = iter(a)
 
-assert(not iterator:all(function(x)
-	return x ~= 2
-end))
+	assert(not iterator:all(function(x)
+		return x ~= 2
+	end))
 
--- we can still use `iter`, as there are more elements.
-assert(next(iterator) == 2)
-```
-]]
-function iter:any(predicate: (any, any) -> boolean)
+	-- we can still use `iter`, as there are more elements.
+	assert(next(iterator) == 2)
+	```
+]=]
+function iter:any(predicate: (...any) -> boolean)
 	for key, value in self do
 		if predicate(self:_getInputTuple(key, value)) then
 			return true
@@ -154,13 +156,13 @@ function iter:any(predicate: (any, any) -> boolean)
 	return false
 end
 
---[[
-Consumes the iterator, counting the number of iterations and returning it.
+--[=[
+	Consumes the iterator, counting the number of iterations and returning it.
 
-This method will call [`next`] repeatedly until `nil` is encountered, returning the
-number of times it saw values. Note that [`next`] has to be called at least once even
-if the iterator does not have any elements.
-]]
+	This method will call [`next`] repeatedly until `nil` is encountered, returning the
+	number of times it saw values. Note that [`next`] has to be called at least once even
+	if the iterator does not have any elements.
+]=]
 function iter:count()
 	local count = 0
 	while self:next() ~= nil do
@@ -170,77 +172,77 @@ function iter:count()
 	return count
 end
 
---[[
-Transforms an iterator into a table.
+--[=[
+	Transforms an iterator into a table.
 
-`collect()` can take anything iterable, and turn it into a relevant table.
-This is one of the more powerful methods in the standard library, used in a variety of contexts.
+	`collect()` can take anything iterable, and turn it into a relevant table.
+	This is one of the more powerful methods in the standard library, used in a variety of contexts.
 
-The most basic pattern in which `collect()` is used is to turn one table into
-another. You take a table, call [`iter`] on it, do a bunch of transformations,
-and then `collect()` at the end.
-]]
+	The most basic pattern in which `collect()` is used is to turn one table into
+	another. You take a table, call [`iter`] on it, do a bunch of transformations,
+	and then `collect()` at the end.
+]=]
 function iter:collect()
 	return self._value
 end
 
---[[
-Creates an iterator which gives the current iteration count as well as the next value.
+--[=[
+	Creates an iterator which gives the current iteration count as well as the next value.
 
-The iterator returned yields pairs `(i, val)` for arrays and `(i, key, val)` for dicts,
-where i is the current index of iteration.
+	The iterator returned yields pairs `(i, val)` for arrays and `(i, key, val)` for dicts,
+	where i is the current index of iteration.
 
-# Examples
-```lua
-local a = {'a', 'b', 'c'}
+	# Examples
+	```lua
+	local a = {'a', 'b', 'c'}
 
-local iterator = array(a).enumerate()
+	local iterator = array(a).enumerate()
 
-print(iterator:next()) -> 1, "a"
-print(iterator:next()) -> 2, "b"
-print(iterator:next()) -> 3, "c"
-print(iterator:next()) -> nil
+	print(iterator:next()) -> 1, "a"
+	print(iterator:next()) -> 2, "b"
+	print(iterator:next()) -> 3, "c"
+	print(iterator:next()) -> nil
 
-local b = {
-	akey = 'a',
-	bkey = 'b',
-	ckey = 'c',
-}
+	local b = {
+		akey = 'a',
+		bkey = 'b',
+		ckey = 'c',
+	}
 
-iterator = dict(b).enumerate()
+	iterator = dict(b).enumerate()
 
-print(iterator:next()) -> 1, "akey", "a"
-print(iterator:next()) -> 2, "bkey", "b"
-print(iterator:next()) -> 3, "ckey", "c"
-print(iterator:next()) -> nil
-```
+	print(iterator:next()) -> 1, "akey", "a"
+	print(iterator:next()) -> 2, "bkey", "b"
+	print(iterator:next()) -> 3, "ckey", "c"
+	print(iterator:next()) -> nil
+	```
 
-# Deviations
-* For dicts, the iterator returned yields tuples `(i, key, val)`
-* Indexing starts at 1
-]]
+	# Deviations
+	* For dicts, the iterator returned yields tuples `(i, key, val)`
+	* Indexing starts at 1
+]=]
 function iter:enumerate()
 	return self._iter(self._value, self._type, true)
 end
 
---[[
-Consumes the iterator, returning the last element.
+--[=[
+	Consumes the iterator, returning the last element.
 
-For non-arrays, this method will evaluate the iterator until it returns `nil`.
-While doing so, it keeps track of the current element. After `nil` is returned,
-`last()` will then return the last element it saw.
+	For non-arrays, this method will evaluate the iterator until it returns `nil`.
+	While doing so, it keeps track of the current element. After `nil` is returned,
+	`last()` will then return the last element it saw.
 
-# Examples
-Basic usage:
+	# Examples
+	Basic usage:
 
-```lua
-local a = {1, 2, 3};
-assert(array(a).last() == 3);
+	```lua
+	local a = {1, 2, 3};
+	assert(array(a).last() == 3);
 
-local a = {1, 2, 3, 4, 5};
-assert(array(a).last() == 5);
-```
-]]
+	local a = {1, 2, 3, 4, 5};
+	assert(array(a).last() == 5);
+	```
+]=]
 function iter:last(): any
 	local res = {}
 	while self:next() ~= nil do
@@ -260,12 +262,12 @@ function iter:map(transformer: (...any) -> any)
 	return self._iter(newTable, self._type, self._enumerate)
 end
 
---[[
-Creates an iterator that both yields elements based on a predicate and maps.
+--[=[
+	Creates an iterator that both yields elements based on a predicate and maps.
 
-`mapWhile()` takes a function as an argument. It will call this function on each element
-of the iterator, and yield elements while it returns non-nil.
-]]
+	`mapWhile()` takes a function as an argument. It will call this function on each element
+	of the iterator, and yield elements while it returns non-nil.
+]=]
 function iter:mapWhile(transformer: (...any) -> any)
 	local newTable = {}
 
@@ -286,12 +288,12 @@ function iter:next()
 	return self:_getInputTuple()
 end
 
---[[
-Creates an iterator which uses a closure to determine if an element should be yielded.
+--[=[
+	Creates an iterator which uses a closure to determine if an element should be yielded.
 
-Given an element the closure must return true or false. The returned iterator will yield
-only the elements for which the closure returns true.
-]]
+	Given an element the closure must return true or false. The returned iterator will yield
+	only the elements for which the closure returns true.
+]=]
 function iter:filter(predicate: (any, any) -> boolean)
 	local newTable = {}
 
@@ -308,31 +310,31 @@ function iter:filter(predicate: (any, any) -> boolean)
 	return self._iter(newTable, self._type, self._enumerate)
 end
 
---[[
-Calls a closure on each element of an iterator.
+--[=[
+	Calls a closure on each element of an iterator.
 
-This is equivalent to using a for loop on the iterator, although break
-and continue are not possible from a closure. It's generally more idiomatic
-to use a for loop, but for_each may be more legible when processing items at
-the end of longer iterator chains.
+	This is equivalent to using a for loop on the iterator, although break
+	and continue are not possible from a closure. It's generally more idiomatic
+	to use a for loop, but for_each may be more legible when processing items at
+	the end of longer iterator chains.
 
-#Examples
-Basic usage:
+	#Examples
+	Basic usage:
 
-```lua
-tb.array({ 0, 1, 2 })
-	:map(function(x: number)
-		return x * 100
-	end)
-	:enumerate()
-	:filter(function(i: number, x: number)
-		return (i + x) % 3 == 0
-	end)
-	:for_each(function(i: number, x: number)
-		print(`{i}:{x}`)
-	end)
-```
-]]
+	```lua
+	tb.array({ 0, 1, 2 })
+		:map(function(x: number)
+			return x * 100
+		end)
+		:enumerate()
+		:filter(function(i: number, x: number)
+			return (i + x) % 3 == 0
+		end)
+		:for_each(function(i: number, x: number)
+			print(`{i}:{x}`)
+		end)
+	```
+]=]
 function iter:forEach(f: (...any) -> ())
 	for key, value in self do
 		f(self:_getInputTuple(key, value))
@@ -340,11 +342,11 @@ function iter:forEach(f: (...any) -> ())
 end
 
 --[[
-For arrays, just returns the value
+	For arrays, just returns the value
 
-For dicts, returns the key and value
+	For dicts, returns the key and value
 
-If enumerated, includes iteration count at the beginning for both arrays and dicts
+	If enumerated, includes iteration count at the beginning for both arrays and dicts
 ]]
 function iter:_getInputTuple(key, value)
 	key = key or self._lastKey
